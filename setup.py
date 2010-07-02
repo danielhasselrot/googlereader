52b8de06ea481046da4d0fa8300f783e03b593df
Daniel Hasselrot <daniel.hasselrot@gmail.com>
2010-07-02 09:59:31 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='googlereader',
-      version='0.1',
+      version='0.1.1',
       description='Fork of google reader api from pyrfeed',
       author=['Tobias Hasselrot', 'Daniel Hasselrot',],
       author_email='daniel.hasselrot@gmail.com',
